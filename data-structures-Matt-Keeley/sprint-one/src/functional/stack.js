b89f0985f927e6dca7c2aa86ed0e3c96cca8f3e4
kiyomi215 <keeleynakamoto@gmail.com>
2016-04-21 15:28:58 -0700

    Stack complete to remove popped value from storage

---
+++ data-structures-Matt-Keeley/sprint-one/src/functional/stack.js
@@ -0,0 +1,28 @@
+var Stack = function(){
+  var someInstance = {};
+  var counter = 0;
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  // Implement the methods below
+  someInstance.push = function(value){
+    storage[counter] = value;
+    counter++;
+  };
+
+  someInstance.pop = function(){
+    if (counter>0){
+      counter--;
+      var results = storage[counter];
+      delete storage[counter];
+      return results;
+    };
+  };
+
+  someInstance.size = function(){
+    return counter;
+  };
+
+  return someInstance;
+};
